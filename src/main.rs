932fc81a02a3911b2c982461c62987616d43f359
Espen Klungland <espen.klungland@gmail.com>
2025-12-07 12:27:52 +0100

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,64 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
+
+#[derive(Debug)]
+struct Rotation {
+    pub direction: char,
+    pub count: i32,
+}
+
+impl Rotation {
+    fn new(dir: char, count: i32) -> Self {
+        Rotation {
+            direction: dir,
+            count,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Lock {
+    pub position: i32,
+    count: HashMap<i32, i32>,
+}
+
+impl Lock {
+    fn new() -> Self {
+        Lock {
+            position: 50,
+            count: HashMap::new(),
+        }
+    }
+
+    fn turn(&mut self, rotation: &Rotation) -> Result<(), &'static str> {
+        match rotation.direction {
+            'L' => {
+                self.position -= rotation.count;
+                if self.position < 0 {
+                    println!("{:?} - {:?}", self.position, rotation);
+                    self.position += 99;
+                }
+                *self.count.entry(self.position).or_insert(0) += 1;
+                Ok(())
+            }
+            'R' => {
+                self.position += rotation.count;
+                if self.position > 99 {
+                    self.position -= 99;
+                }
+                *self.count.entry(self.position).or_insert(0) += 1;
+                Ok(())
+            }
+            _ => Err("Got invalid direction"),
+        }
+    }
+
+    fn get_count(&self, key: i32) -> Option<&i32> {
+        self.count.get(&key)
+    }
+}
 
 fn parse_input(path: &Path) -> io::Result<std::io::Lines<BufReader<File>>> {
     let file = File::open(path)?;
@@ -9,21 +67,29 @@ fn parse_input(path: &Path) -> io::Result<std::io::Lines<BufReader<File>>> {
     Ok(reader.lines())
 }
 
-fn parse_line(line: &str) {
+fn parse_line(line: &str) -> Rotation {
     let mut chars = line.chars();
-    let direction = chars.next().unwrap().to_string();
-    let count: i8 = chars.collect::<String>().parse().unwrap();
-    println!("{}", direction);
-    println!("{}", count);
+    let direction = chars.next().unwrap();
+    let count: i32 = chars.collect::<String>().parse().unwrap();
+    Rotation::new(direction, count)
 }
 
 fn main() {
     let input_file = Path::new("input/day1.txt");
     let input = parse_input(input_file).expect("failed to parse input");
 
+    let mut lock = Lock::new();
+
     for line in input {
         let line = line.expect("failed to read line");
-        parse_line(&line);
-        // println!("{}", line);
+        let rotation = parse_line(&line);
+        // println!("{:?}", rotation);
+        lock.turn(&rotation).expect("Failed to turn lock");
+    }
+
+    if let Some(count) = lock.get_count(0) {
+        println!("Lock at position 0 count: {}", count);
+    } else {
+        println!("Lock never hit 0...");
     }
 }
