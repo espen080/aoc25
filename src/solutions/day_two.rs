b0aa5a1d7a98c85d6648b66303282aeeb15f2a67
Espen Klungland <espen.klungland@gmail.com>
2025-12-26 12:49:38 +0100

    day 2 part 1

--- src/solutions/day_two.rs
+++ src/solutions/day_two.rs
@@ -16,15 +16,48 @@ impl Range {
             max: end.parse().unwrap(),
         }
     }
-    fn find_invalid_ids(&mut self) {
-        println!("{} {}", self.start, self.end);
-        for i in self.min..self.max {
-            println!("{}", i);
+    fn sum_invalid_ids(&mut self) -> u64 {
+        /*
+         * Since the young Elf was just doing silly patterns, you can find the
+         * invalid IDs by looking for any ID which is made only of some sequence
+         * of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and
+         * 123123 (123 twice) would all be invalid IDs.
+         */
+        let mut sum = 0;
+        for i in self.min..self.max + 1 {
+            let str_i = i.to_string();
+            let (lhs, rhs) = str_i.split_at(str_i.len() / 2);
+            if lhs == rhs {
+                sum += i;
+            }
         }
+        sum
+    }
+    fn sum_more_invalid_ids(&mut self) -> u64 {
+        /*
+         * Now, an ID is invalid if it is made only of some sequence of digits
+         * repeated at least twice. So, 12341234 (1234 two times),
+         * 123123123 (123 three times), 1212121212 (12 five times),
+         * and 1111111 (1 seven times) are all invalid IDs.
+         */
+        let mut sum = 0;
+        for i in self.min..self.max + 1 {
+            let str_i = i.to_string();
+            let max_mask_size = str_i.len() / 2;
+            for j in 0..max_mask_size + 1 {
+                let (lhs, rhs) = str_i.split_at(j);
+                if lhs == rhs {
+                    sum += i;
+                }
+            }
+        }
+        sum
     }
 }
 
 pub fn solution() {
+    let mut sum = 0;
+    let mut other_sum = 0;
     let input = parse_input("input/day2.txt").expect("failed to read input");
     for line in input {
         let line = line.expect("failed to read line");
@@ -32,7 +65,10 @@ pub fn solution() {
         for range in ranges {
             let (start, end) = range.split_once("-").expect("failed to split range");
             let mut range = Range::new(start, end);
-            range.find_invalid_ids();
+            sum += range.sum_invalid_ids();
+            other_sum += range.sum_more_invalid_ids();
         }
     }
+    println!("Total sum {}", sum);
+    println!("Even more total sum {}", other_sum);
 }
