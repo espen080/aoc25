36fc08a1a551404b9eb4b3621647707c6c92ca65
Espen Klungland <espen.klungland@gmail.com>
2025-12-28 20:05:32 +0100

    refactor day 3 part 1

--- src/solutions/day_three.rs
+++ src/solutions/day_three.rs
@@ -1,18 +1,44 @@
 use crate::utils::parse_input;
 use std::simd::{prelude::SimdPartialEq, u8x64};
 
-const NINES_MASK: u8x64 = u8x64::splat(b'9');
-
 fn parse_line(line: &str) -> u8 {
-    let lhs = &line[..line.len() / 2];
-    let rhs = &line[line.len() / 2..];
-    for i in 9..0 {
-        println!("{}", i);
+    let offset = line.len() / 2;
+    let lhs = &line[..offset];
+    let rhs = &line[offset..];
+    for i in (0..=9).rev() {
+        if let Some(result) = simd_parse(lhs, i + b'0') {
+            println!("{:?}", result);
+        }
+        if let Some(mut result) = simd_parse(rhs, i + b'0') {
+            result.index += offset as u8;
+            println!("{:?}", result);
+        }
     }
-    let lhs_mask: u8x64 = u8x64::load_or_default(lhs.as_bytes());
-    let res = lhs_mask.simd_eq(NINES_MASK);
-    let count = res.to_bitmask().count_ones() as u8;
-    count
+    0
+}
+
+#[derive(Debug)]
+struct ParseResult {
+    pub value: char,
+    pub count: u8,
+    pub index: u8,
+}
+
+fn simd_parse(input: &str, i: u8) -> Option<ParseResult> {
+    let target = u8x64::splat(i);
+    let chunk = u8x64::load_or_default(input.as_bytes());
+    let mask = chunk.simd_eq(target);
+    if mask.any() {
+        let bits = mask.to_bitmask();
+        let index = bits.trailing_zeros() as u8;
+        let count = bits.count_ones() as u8;
+        return Some(ParseResult {
+            value: i as char,
+            count,
+            index,
+        });
+    }
+    None
 }
 
 pub fn solution() {
@@ -30,6 +56,7 @@ mod tests {
     use super::*;
     #[test]
     fn test_parse() {
-        assert_eq!(parse_line("193456789"), 17);
+        assert_eq!(parse_line("123456789"), 89);
+        assert_eq!(parse_line("987654321"), 98);
     }
 }
